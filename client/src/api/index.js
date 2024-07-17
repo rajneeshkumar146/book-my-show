4769d1ace995a55a1753507d9dba89f74c945e36
Rajneesh Kumar <rajneeshkumar@Rajneeshs-MacBook-Pro.local>
2024-07-17 08:50:09 +0530

    updated_localHostTo5000_dependency

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "http://localhost:5000/",
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${localStorage.getItem("token")}`,
