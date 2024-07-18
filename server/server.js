9ae83b94794c790c548638906b27dfb92cfcd24e
Rajneesh Kumar <rajneeshkumar@Rajneeshs-MacBook-Pro.local>
2024-07-18 19:42:18 +0530

    updated_port_and_baseUrl

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.use((req, res) => {
     res.status(404).json({ message: "Page not found" });
 });
 
-const PORT = proccess.env.PORT;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log("Server is running on port PORT");
 })
